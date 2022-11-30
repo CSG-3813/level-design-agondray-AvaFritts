fd21a52a3359b68f70d88ac7f1844205be0955d2
AvaFritts <avanfritts@gmail.com>
2022-11-30 17:04:48 -0600

    Started Branch

--- UnityAgonDray/Assets/Scripts/Chest.cs
+++ UnityAgonDray/Assets/Scripts/Chest.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Chest : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         
     }
--- UnityAgonDray/Assets/Scripts/Sword.cs
+++ UnityAgonDray/Assets/Scripts/Sword.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    public string playerTag;
+    private Animator animator;
+    //private AudioSource audioSource;
+    public bool hasCollected = false;
+    public bool canCollect = false;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        animator = this.GetComponent<Animator>();
+        //audioSource = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (canCollect)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetTrigger("Collect");
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            animator.SetBool("InArea", true);
+            canCollect = true;
+        }
+    }
+
+    void CollectSword() 
+    {
+        hasCollected = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            if (!hasCollected)
+            {
+                animator.SetBool("InArea", false);
+                canCollect = false;
+            }
+        }
     }
 }
